41a7af2e2e64ec079154d08e213dbd4ef2a1aec3
Timothy DeHerrera <tim@nrdxp.dev>
2024-08-13 14:12:27 -0600

    feat: wire up initial deps and init logger
    * tokio: for async runtime
    * tracing: for logging in an async friendly fashion
    * anyhow & thiserror: simplified error reportin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::registry()
+        .with(fmt::layer())
+        .with(EnvFilter::from_default_env())
+        .init();
+
+    Ok(())
 }
