5814d87f70d2de75ea677c8526fcf1013214c9a6
Timothy DeHerrera <tim@nrdxp.dev>
2024-09-02 21:51:26 -0600

    fix(uri): correct parsing for user:pass
    correctly identify a user:pass@ connection string in a url, and do not
    misidentify it as an alias. Also, adds a fix for ssh style string which
    do not contain the `@` symbol, but only a domain.
    
    To be clear, an alias cannot contain a `.` character, and for clarity
    must at least specify a full domain, which will be checked in future
    code.
    
    Also includes the snapshots for the tests, which were accidentally
    omitted from the previous commit.

--- src/cli/uri/mod.rs
+++ src/cli/uri/mod.rs
@@ -2,7 +2,7 @@ use nom::{
     branch::alt,
     bytes::complete::{tag, take_until, take_while},
     character::complete::digit1,
-    combinator::{map, not, opt, peek, verify},
+    combinator::{map, not, opt, peek, recognize, verify},
     sequence::tuple,
     IResult,
 };
@@ -37,11 +37,20 @@ impl<'a> From<&'a str> for UriRef<'a> {
         let alias = |input: &'a str| {
             opt(map(
                 verify(
-                    tuple((take_until(":"), tag(":"), peek(not(alt((digit1,)))))),
+                    tuple((
+                        take_until(":"),
+                        tag(":"),
+                        peek(not(alt((
+                            // not a port
+                            digit1,
+                            // not a user:pass@example.com
+                            recognize(tuple((take_until("@"), take_until(".")))),
+                        )))),
+                    )),
                     |(before_colon, _, _)| {
                         #[allow(clippy::explicit_auto_deref)]
-                        // Fail if it's an SSH-style URL
-                        !<str>::contains(*before_colon, '@')
+                        // not an SSH-style url
+                        <str>::find(*before_colon, |c| c == '@' || c == '.').is_none()
                     },
                 ),
                 |(alias, _, _)| alias,
@@ -85,13 +94,18 @@ mod tests {
     #[test]
     fn uri_snapshot() {
         let results: Vec<UriRef> = vec![
+            "alias:path.with/dot//my/atom@^2".into(),
+            "git@github.com:owner/repo//path/to/atom@^1".into(),
+            "https://example.com/owner/repo:8080//path/to/atom@^1".into(),
             "https://github.com/owner/repo//path/to/atom@^1".into(),
             "https://hub:owner/repo//path/to/atom@^1".into(),
+            "https://user:password@example.com/my/repo//atom/path@^0.2".into(),
             "hub:owner/repo//path/to/atom@^1".into(),
-            "git@github.com:owner/repo//path/to/atom@^1".into(),
+            // not an alias, but an ssh url without a username
+            "my.ssh.com:my/repo//path/to/atom".into(),
+            "/path/to/atom@^0.8".into(),
             "///path/to/atom".into(),
             "//path/to/atom".into(),
-            "/path/to/atom@^0.8".into(),
         ];
         insta::assert_yaml_snapshot!(results);
     }
