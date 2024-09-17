42a90958003e4173af69b08d0b904ef394e1b31a
Timothy DeHerrera <tim@nrdxp.dev>
2024-09-17 17:42:54 -0600

    feat: init basic config crate

---
+++ crates/config/src/lib.rs
@@ -0,0 +1,54 @@
+use etcetera::BaseStrategy;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+
+use figment::{
+    providers::{Env, Format, Toml},
+    Figment, Metadata, Provider,
+};
+
+#[derive(Deserialize, Serialize)]
+pub struct Config {
+    aliases: HashMap<String, String>,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Config {
+            aliases: HashMap::from_iter([
+                ("gh".into(), "github.com".into()),
+                ("gl".into(), "gitlab.com".into()),
+                ("bb".into(), "bitbucket.org".into()),
+                ("sf".into(), "git.code.sf.net".into()),
+            ]),
+        }
+    }
+}
+
+impl Config {
+    pub fn from<T: Provider>(provider: T) -> Result<Config, figment::Error> {
+        Figment::from(provider).extract()
+    }
+
+    pub fn figment() -> Figment {
+        let mut fig = Figment::from(Config::default());
+
+        if let Ok(c) = etcetera::choose_base_strategy() {
+            let config = c.config_dir().join("eka.toml");
+            fig = fig.adjoin(Toml::file(config));
+        }
+
+        fig.admerge(Env::prefixed("EKA_"))
+    }
+}
+
+impl Provider for Config {
+    fn metadata(&self) -> figment::Metadata {
+        Metadata::named("Eka CLI Config")
+    }
+    fn data(
+        &self,
+    ) -> Result<figment::value::Map<figment::Profile, figment::value::Dict>, figment::Error> {
+        figment::providers::Serialized::defaults(Config::default()).data()
+    }
+}
