8f22ac836a4b2ca826b750a5f5abf0b2b4409b60
Timothy DeHerrera <tim@nrdxp.dev>
2024-10-01 13:01:33 -0600

    feat: experimental nix evaluator isolator

---
+++ crates/nixec/src/main.rs
@@ -0,0 +1,82 @@
+use birdcage::{process::Command, Birdcage, Exception, Sandbox};
+use std::process::ExitCode;
+use std::{
+    env, fs,
+    path::{Path, PathBuf},
+    process::Command as UnsafeCommand,
+};
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+enum NixecError {
+    #[error("No `nix` executable in PATH")]
+    NoNix,
+    #[error(transparent)]
+    ExceptionFailed(#[from] birdcage::error::Error),
+    #[error(transparent)]
+    CommandFailed(#[from] std::io::Error),
+    #[error("Failed to determine nix store path")]
+    StorePath,
+}
+
+type Result<T> = std::result::Result<T, NixecError>;
+
+fn main() -> Result<ExitCode> {
+    let nix_dir = bin_dir("nix")?;
+    let nix_instantiate = nix_dir.join("nix-instantiate");
+    let cwd = env::current_dir()?;
+
+    let args: Vec<String> = env::args().collect();
+    let sandbox_args = &args[1..];
+
+    let mut sandbox = Birdcage::new();
+
+    sandbox.add_exception(Exception::Read(cwd))?;
+
+    let nix_store: PathBuf = String::from_utf8(
+        UnsafeCommand::new(nix_instantiate.clone())
+            .args(["--eval", "--expr", "builtins.storeDir"])
+            .output()?
+            .stdout,
+    )
+    .map_err(|_| NixecError::StorePath)?
+    .trim()
+    .trim_matches('"')
+    .into();
+
+    sandbox.add_exception(Exception::ExecuteAndRead(
+        nix_store
+            .parent()
+            .map(Path::to_path_buf)
+            .ok_or(NixecError::StorePath)?,
+    ))?;
+    unsafe { env::set_var("HOME", "/homeless-shelter") };
+    sandbox.add_exception(Exception::Environment("HOME".into()))?;
+
+    sandbox.add_exception(Exception::ExecuteAndRead(nix_dir))?;
+    let mut command = Command::new(nix_instantiate);
+    command.args(sandbox_args);
+
+    let output = sandbox.spawn(command)?.wait_with_output()?;
+
+    Ok(ExitCode::from(output.status.code().unwrap_or(1) as u8))
+}
+
+fn bin_dir(exec_name: &str) -> Result<PathBuf> {
+    env::var_os("PATH")
+        .and_then(|paths| {
+            env::split_paths(&paths)
+                .filter_map(|dir| {
+                    let full_path = dir.join(exec_name);
+                    if full_path.is_file() {
+                        fs::canonicalize(full_path)
+                            .ok()
+                            .and_then(|p| p.parent().map(Path::to_path_buf))
+                    } else {
+                        None
+                    }
+                })
+                .next()
+        })
+        .ok_or(NixecError::NoNix)
+}
